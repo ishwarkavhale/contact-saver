4bb503db92bcbcf113edbbc642e555d3ad1aab13
ishwar <contactkepper.com>
2020-07-18 09:56:55 +0530

    User created and stored in database

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { validationResult, check } = require('express-validator');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { findOne } = require('../models/User');
 
 // @route   POST api/user
 // @desc    Register User
@@ -17,12 +19,31 @@ router.post(
       'Please Enter Password with minimum length of 6.'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exist.' });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      user.save();
+      res.send('user saved');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
