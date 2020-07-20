5c3ab44c3b590b884facfbbe71af9a1f0d9069d6
ishwar <contactkepper.com>
2020-07-20 08:29:39 +0530

    User looged in with middleware auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,22 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const config = require('config');
 const router = express.Router();
+const auth = require('../Middleware/auth');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  private
 
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    console.log(user);
+
+    res.json({ user });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/auth
